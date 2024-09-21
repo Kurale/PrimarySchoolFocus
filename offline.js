a2ae6a43a1d36134f03dc144c7472961d603af62
Kurale <89684634+Kurale@users.noreply.github.com>
2024-09-21 11:07:56 +0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1726740738,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/kostik_1-sheet0.png",
+		"images/kostik_10_1-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/knopka-sheet0.png",
+		"images/knopka-sheet1.png",
+		"media/click_5.m4a",
+		"media/click_5.ogg",
+		"media/small_whoosh_fast_10.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
